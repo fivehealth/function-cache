32400580b32b1eee5d8bd9b5d5fb6252759f2df9
yc <yc@botmd.io>
2019-08-22 20:24:40 +0800

    Rename project

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
 setup(
-    name='django-function-cache',
+    name='function-cache',
     version='0.1',
     packages=['function_cache'],
-    description='A convenience decorator for caching results of functions to various backends.',
+    description='A Django-based convenience decorator for caching results of functions to various backends.',
     long_description=README,
     author='Yanchuan Sim',
     author_email='yc@botmd.io',
-    url='https://gitlab.com/fivehealth/django-function-cache',
+    url='https://gitlab.com/fivehealth/function-cache',
     license='Apache License 2.0',
     install_requires=[
         'Django>2.0',
